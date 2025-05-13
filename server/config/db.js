54f654816eb5be58c72ece930d51f2cd985da674
Sachin Rathore <12sachin95@gmail.com>
2025-05-13 16:05:44 +0530

    FIX: db URI updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  // const conn = await mongoose.connect(process.env.MONGO_URI);
+  // const conn = await mongoose.connect("mongodb://mongo:27017/test");
+
+  // connect using localhost if running mongodb through docker
+  const conn = await mongoose.connect("mongodb://localhost:27017/mydb");
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
